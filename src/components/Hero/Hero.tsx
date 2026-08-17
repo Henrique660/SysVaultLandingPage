df3448e52f7c50c3b939bf43d643a4b4970f1ceb
Henrique_Silva <devhenri.silva@gmail.com>
2026-08-17 00:00:06 -0300

    .

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,6 +4,7 @@ import "./Marquee.css";
 
 export function Hero() {
   return (
+    /*teste*/
     <section className="hero" id="apresentacao">
       <div className="container">
         <div className="hero-grid">
